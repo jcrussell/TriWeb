8a9888d9f4ffcb3b2d5db664c18c3f1e66b716e7
Jon Crussell <joncrussell@gmail.com>
2011-08-24 23:11:57 -0700

    inital commit of default RoR app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TriWeb::Application.config.secret_token = '15583c856a7d56a9f5996eff3ae83dfe4eb00e760cf59bab8c53566019ee2a886233fd878e684e101628c53cf931e705b325bf12adba062fe49aaa61d574988c'
