bd7b47a10f8311ced94e063492fab83299d50fd3
Jon Crussell <joncrussell@gmail.com>
2011-09-04 10:30:36 -0700

    added calendar navigation

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,12 +2,16 @@ class CalendarController < ApplicationController
   before_filter :authenticate_user!
 
   # GET /calendar
+  # GET /calendar?date=YYYY-MM-DD
   def index
+    @date = Date.parse(params[:date]) unless params[:date].nil?
+    @date ||= Date.today
+
     # Get workouts ranging from 3 days ago to 10 days in advance => 2 week window.
-    # TODO: User should be select calendar mode - fixed 2 weeks, month, week etc.
-    # TODO: User should be able to navigate forwards and backwards weeks
-    first = 3.day.ago(Date.today)
-    last = 10.day.from_now(Date.today)
+    # TODO: User should be able to select calendar mode - fixed 2 weeks, month, week etc.
+    first = 3.day.ago(@date)
+    last = 10.day.from_now(@date)
+
     @workouts = Workout.find_by_range(first, last)
 
     # Add class to each: past, today, future
@@ -27,5 +31,9 @@ class CalendarController < ApplicationController
     days = %w(Sunday Monday Tuesday Wednesday Thursday Friday Saturday)
     # Get the list of days of the week, only need the first 7
     @days_of_week = (first..last).collect {|x| days[x.wday]}.uniq
+
+    # Calendar navigation
+    @next_group = 14.days.from_now(@date)
+    @prev_group = 14.days.ago(@date)
   end
 end
